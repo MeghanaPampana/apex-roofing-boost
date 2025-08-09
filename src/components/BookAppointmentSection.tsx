c64362bb8f7a6781ddfe7d1f98db87442f908d36
Meghana Pampana <meghanapampana@gmail.com>
2025-08-09 22:04:28 +0530

    changes

--- src/components/BookAppointmentSection.tsx
+++ src/components/BookAppointmentSection.tsx
@@ -10,9 +10,8 @@ const BookAppointmentSection = () => {
           background: "radial-gradient(circle, #128000 0%, #000000 100%)",
         }}
       ></div>
-      <div className="max-w-6xl mx-auto px-4 relative z-10 flex items-center justify-center min-h-[80vh]">
-        {/* <div className="max-w-6xl mx-auto mb-20"> */}
-          <Card className="glass-card transition-all duration-500 w-full max-w-8xl min-h-[500px] flex items-end justify-center" style={{ border: "2px solid white" }}>
+      <div className="max-w-5xl mx-auto px-4 relative z-10 flex items-center justify-center min-h-[80vh]">      
+          <Card className=" transition-all duration-500 w-full max-w-8xl min-h-[500px] flex items-end justify-center" style={{ border: "1px solid white" }}>
             <CardContent className="p-16">
               <div className="space-y-8 text-foreground leading-relaxed text-center">
                 <h2 className="text-3xl md:text-5xl font-bold text-center mb-13 text-white tracking-tight">
@@ -24,8 +23,8 @@ const BookAppointmentSection = () => {
                   <Button 
                     variant="gradient" 
                     size="lg" 
-                    className="px-16 py-9"
-                    style={{ border: "2px solid white", color: "white" }}
+                    className="px-16 py-7"
+                    style={{ border: "1px solid white", color: "white" }}
                   >
                     Book an Appointment
                   </Button>
